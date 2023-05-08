203c2eebadcb694714926f7e52205666096b6009
Qrypt <52085434+qryptdev@users.noreply.github.com>
2023-05-08 10:46:39 -0400

    Update quickstarts for SDK 0.10.2 #34 (#36)
    * Initial commit
    
    * Minor fix
    
    ---------
    
    Co-authored-by: Andrew Hayman <andrewh@qrypt.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sdk_file='qrypt-security-ubuntu.tgz'
-curl -s https://qrypt.azureedge.net/sdk/cpp/v0.9.2/qrypt-security-0.9.2-ubuntu.tgz --output $sdk_file
+curl -s https://qrypt.azureedge.net/sdk/cpp/v0.10.2/qrypt-security-0.10.2-ubuntu.tgz --output $sdk_file
 tar -zxvf $sdk_file --strip-components=1 -C QryptSecurity
 rm -rf $sdk_file
 
