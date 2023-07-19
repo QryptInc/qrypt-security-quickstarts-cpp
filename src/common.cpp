21a236ed143751840e7d4eaefc0c491ca224a99d
Spencer Vatrt-Watts <spencer@qrypt.com>
2023-07-19 16:25:55 -0400

    Update common.cpp

--- src/common.cpp
+++ src/common.cpp
@@ -139,5 +139,5 @@ void uploadFileToCodespace(const std::string& filename, const std::string& codes
     // send request
     curlRequest(url, filename.c_str(), empty);
 
-    std::cout << "File uploaded successfully to the remote codespace at " << std::endl;
+    std::cout << "File uploaded successfully to the remote codespace" << std::endl;
 }
